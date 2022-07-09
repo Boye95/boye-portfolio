9155d1bb22b7bb663a2f536cddc382867f875110
Boye <samuel.boye95@yahoo.com>
2022-07-10 00:30:28 +0100

    add initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import { About, Contact, Experience, Footer, Header, Nav, Portfolio, Services, Testimonials } from './components';
+
+
+/**
+ * The App function returns the Header, Nav, About, Experience, Services, Portfolio, Testimonials,
+ * Contact, and Footer components
+ * @returns The Header, Nav, About, Experience, Services, Portfolio, Testimonials, Contact, and Footer
+ * components are being returned.
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
