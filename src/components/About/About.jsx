068f4aa3d9ad319a73e3524876f0f84b82d5107e
Boye <samuel.boye95@yahoo.com>
2022-07-11 23:40:58 +0100

    add about section WIP

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
-import './About.scss';
+import './About.scss'
+
+import ME from '../../files/boyeside.png'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+      <div className='main-container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='About Image' />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>60+ Clients</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>50+ Projects</small>
+            </article>
+          </div>
+          <p>
+            Front-End developer with a proven ability to collaborate effectively
+            with senior developers while spending extra time to be mentored.
+            Enjoy working closely with team members to ensure work is done
+            effectively and efficiently and picking up the slack when necessary.
+            With over 2 years of experience in customer service and
+            satisfaction, I have developed and imbibed a customer-first approach
+            in my code implementations. I'm looking forward to joining a team
+            that helps sharpen my skills and improve my capabilities.
+          </p>
+          <a href='#contact' className='btn btn-primary'>
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
   )
 }
 
