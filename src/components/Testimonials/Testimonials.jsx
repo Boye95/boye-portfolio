ac4059bc59b9dcc3013d2d4ba276e3237e975749
Boye <samuel.boye95@yahoo.com>
2022-07-18 23:40:10 +0100

    add done testimonial section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
-import './Testimonials.scss';
+import React from 'react'
+import './Testimonials.scss'
+
+import Avatar1 from '../../files/testimony.png'
+import Avatar2 from '../../files/testimony.png'
+import Avatar3 from '../../files/testimony.png'
+import Avatar4 from '../../files/testimony.png'
+import { Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 const Testimonials = () => {
+  const data = [
+    {
+      avatar: Avatar1,
+      name: 'Ben Jackizz',
+      review:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque beatae culpa dolorem error eum, expedita illo laboriosam magni mollitia perferendis provident quae quaerat quidem quod sint? Consectetur minima necessitatibus quam?'
+    },
+    {
+      avatar: Avatar2,
+      name: 'Hugh  Remi',
+      review:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque beatae culpa dolorem error eum, expedita illo laboriosam magni mollitia perferendis provident quae quaerat quidem quod sint? Consectetur minima necessitatibus quam?'
+    },
+    {
+      avatar: Avatar3,
+      name: 'Ajayi Ogidiolu',
+      review:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque beatae culpa dolorem error eum, expedita illo laboriosam magni mollitia perferendis provident quae quaerat quidem quod sint? Consectetur minima necessitatibus quam?'
+    },
+    {
+      avatar: Avatar4,
+      name: 'Baba Sule',
+      review:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque beatae culpa dolorem error eum, expedita illo laboriosam magni mollitia perferendis provident quae quaerat quidem quod sint? Consectetur minima necessitatibus quam?'
+    }
+  ]
+
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id={'testimonials'}>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className='main-container testimonials__container'
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={true}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className='testimonial'>
+              <div className='client__avatar'>
+                <img src={avatar} alt='Client Image' />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+            </SwiperSlide>
+          )
+          })}
+      </Swiper>
+    </section>
   )
 }
 
