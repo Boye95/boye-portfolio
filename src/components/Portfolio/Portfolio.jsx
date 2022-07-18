1d34aa4cf4b9c8e76996bf6811193ce33b9324bd
Boye <samuel.boye95@yahoo.com>
2022-07-18 22:41:24 +0100

    add portfolio section done

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,131 +1,87 @@
 import React from 'react'
 
 import './Portfolio.scss'
-// // import IMG1 from '../../assets/portfolio1.jpg'
-// // import IMG2 from '../../assets/portfolio2.jpg'
-// // import IMG3 from '../../assets/portfolio3.jpg'
-// // import IMG4 from '../../assets/portfolio4.jpg'
-// // import IMG5 from '../../assets/portfolio5.png'
-// // import IMG6 from '../../assets/portfolio6.jpg'
+import IMG1 from '../../files/finite.PNG'
+import IMG2 from '../../files/hometeach.PNG'
+import IMG3 from '../../files/fuelerUI.PNG'
+import IMG4 from '../../files/reactdoc.PNG'
+import IMG5 from '../../files/calculator.PNG'
+import IMG6 from '../../files/moneybuddy.PNG'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'FiniteNFT: an NFT Marketplace with wide range of arts.',
+    github: 'https://github.com/Boye95/FiniteNFT',
+    demo: 'https://boyefinitenft.netlify.app/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title:
+      'HomeTeach: a platform for experienced teachers to register and retail their services.',
+    github: 'https://github.com/Boye95/home-teach-pjt-54-fe',
+    demo: 'https://hometeachv1.netlify.app/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title:
+      'FuelerUI: a platform that helps developers show their proof of work',
+    github: 'https://github.com/Boye95/fuelerUI',
+    demo: 'https://boye95.github.io/fuelerUI/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'React JS documentation',
+    github: 'https://github.com/Boye95/React-Documentation',
+    demo: 'https://codepen.io/boye95/full/zYEWdVM'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Calculaor: this is a simple JavaScript calculator',
+    github: 'https://github.com/Boye95/JS-Calculator',
+    demo: 'https://boye95.github.io/JS-Calculator/'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'MoneyBuddyUI: a platform for students to earn while learning',
+    github: 'https://github.com/Boye95/MoneyBuddyUI',
+    demo: 'https://moneybuddy.netlify.app/'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <div>bh</div>
-//     <section id={'portfolio'}>
-//       <h5>My Recent Work</h5>
-//       <h2>Portfolio</h2>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-//       <div className={'container portfolio__container'}>
-//         <article className={'portfolio__item'}>
-//           <div className={'portfolio__item-image'}>
-//             <img src={IMG1} alt={''} />
-//           </div>
-//           <h3>This is a portfolio item title</h3>
-//           <div className={'portfolio__item-cta'}>
-//             <a href={'https://github.com'} className={'btn'} target={'_blank'}>
-//               Github
-//             </a>
-//             <a
-//               href={'https://dribbble.com'}
-//               className={'btn btn-primary'}
-//               target={'_blank'}
-//             >
-//               Live Demo
-//             </a>
-//           </div>
-//         </article>
-//         <article className={'portfolio__item'}>
-//           <div className={'portfolio__item-image'}>
-//             <img src={IMG2} alt={''} />
-//           </div>
-//           <h3>This is a portfolio item title</h3>
-//           <div className={'portfolio__item-cta'}>
-//             <a href={'https://github.com'} className={'btn'} target={'_blank'}>
-//               Github
-//             </a>
-//             <a
-//               href={'https://dribbble.com'}
-//               className={'btn btn-primary'}
-//               target={'_blank'}
-//             >
-//               Live Demo
-//             </a>
-//           </div>
-//         </article>
-//         <article className={'portfolio__item'}>
-//           <div className={'portfolio__item-image'}>
-//             <img src={IMG3} alt={''} />
-//           </div>
-//           <h3>This is a portfolio item title</h3>
-//           <div className={'portfolio__item-cta'}>
-//             <a href={'https://github.com'} className={'btn'} target={'_blank'}>
-//               Github
-//             </a>
-//             <a
-//               href={'https://dribbble.com'}
-//               className={'btn btn-primary'}
-//               target={'_blank'}
-//             >
-//               Live Demo
-//             </a>
-//           </div>
-//         </article>
-//         <article className={'portfolio__item'}>
-//           <div className={'portfolio__item-image'}>
-//             <img src={IMG4} alt={''} />
-//           </div>
-//           <h3>This is a portfolio item title</h3>
-//           <div className={'portfolio__item-cta'}>
-//             <a href={'https://github.com'} className={'btn'} target={'_blank'}>
-//               Github
-//             </a>
-//             <a
-//               href={'https://dribbble.com'}
-//               className={'btn btn-primary'}
-//               target={'_blank'}
-//             >
-//               Live Demo
-//             </a>
-//           </div>
-//         </article>
-//         <article className={'portfolio__item'}>
-//           <div className={'portfolio__item-image'}>
-//             <img src={IMG5} alt={''} />
-//           </div>
-//           <h3>This is a portfolio item title</h3>
-//           <div className={'portfolio__item-cta'}>
-//             <a href={'https://github.com'} className={'btn'} target={'_blank'}>
-//               Github
-//             </a>
-//             <a
-//               href={'https://dribbble.com'}
-//               className={'btn btn-primary'}
-//               target={'_blank'}
-//             >
-//               Live Demo
-//             </a>
-//           </div>
-//         </article>
-//         <article className={'portfolio__item'}>
-//           <div className={'portfolio__item-image'}>
-//             <img src={IMG6} alt={''} />
-//           </div>
-//           <h3>This is a portfolio item title</h3>
-//           <div className={'portfolio__item-cta'}>
-//             <a href={'https://github.com'} className={'btn'} target={'_blank'}>
-//               Github
-//             </a>
-//             <a
-//               href={'https://dribbble.com'}
-//               className={'btn btn-primary'}
-//               target={'_blank'}
-//             >
-//               Live Demo
-//             </a>
-//           </div>
-//         </article>
-//       </div>
-//     </section>
+      <div className='main-container portfolio__container'>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className='portfolio__item'>
+              <div className='portfolio__item-image'>
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn github' target='_blank'>
+                  Github
+                </a>
+                <a href={demo} className='btn btn-primary live' target='_blank'>
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 export default Portfolio
